d0bbabc5e8f09e04501574b44c5fce1b19ebf1bb
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-09-22 09:37:52 +0200

    Extending EmailsListBlock query

---
+++ Entity/Repository/ContactRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Librinfo\EmailCRMBundle\Entity\Repository;
+
+//use Librinfo\CRMBundle\Entity\Repository\ContactRepository as BaseContactRepository;
+use Doctrine\ORM\EntityRepository;
+
+class ContactRepository extends EntityRepository
+{
+
+}
---
+++ Entity/Repository/OrganismRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Librinfo\EmailCRMBundle\Entity\Repository;
+
+use Librinfo\CRMBundle\Entity\Repository\OrganismRepository as BaseOrganismRepository;
+
+class OrganismRepository extends BaseOrganismRepository
+{
+    /**
+     * @param string $id  the Organism id
+     */
+    public function getEmailMessagesQueryBuilder($id)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder('e');
+        $qb->select('e')
+            ->from('LibrinfoEmailBundle:Email', 'e')
+            ->leftJoin ('e.organisms', 'org')
+            ->leftJoin ('e.positions', 'pos')
+            ->where('org.id = :id')
+            ->orWhere($qb->expr()->andX(
+                $qb->expr()->eq('pos.organism', ':id'),
+                $qb->expr()->isNotNull('pos.id')))
+            ->setParameter('id', $id)
+        ;
+        return $qb;
+    }
+}
