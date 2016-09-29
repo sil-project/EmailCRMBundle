574efe7f001ae7ecb1527786357ac25c950c7190
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-09-29 14:43:42 +0200

    Normalized librinfo sonata admin codes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('librinfo_email_crm');
+        $rootNode = $treeBuilder->root('librinfo_emailcrm');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
