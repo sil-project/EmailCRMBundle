2844da881fdfb06620c0c85af0bb088346ae271a
Romain <romain.sanchez@libre-informatique.fr>
2016-10-21 15:38:39 +0200

    Extended EmailBundle DbSpool service to include replacements processing

---
+++ Services/SwiftMailer/Spool/DbSpool.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Librinfo\EmailCRMBundle\Services\SwiftMailer\Spool;
+
+use Librinfo\EmailBundle\Services\SwiftMailer\Spool\DbSpool as BaseDbSpool;
+use Librinfo\EmailCRMBundle\Services\SwiftMailer\DecoratorPlugin\Replacements;
+
+/**
+ * Class DbSpool
+ */
+class DbSpool extends BaseDbSpool
+{
+    public function flushQueue(\Swift_Transport $transport, &$failedRecipients = null)
+    {
+        $replacements = new Replacements($this->manager);
+        $decorator = new \Swift_Plugins_DecoratorPlugin($replacements);
+        $transport->registerPlugin($decorator);
+        
+        parent::flushQueue($transport, $failedRecipients);
+    }
+}
