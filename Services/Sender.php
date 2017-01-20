9722d887837736e7b36f6df5b75bba13c457c5f2
Romain <romain.sanchez@libre-informatique.fr>
2017-01-20 09:37:31 +0100

    Fixed Sender service

--- Services/Sender.php
+++ Services/Sender.php
@@ -7,14 +7,13 @@ use Librinfo\EmailCRMBundle\Services\SwiftMailer\DecoratorPlugin\Replacements;
 
 class Sender extends BaseSender
 {
-    protected function directSend($to, &$failedRecipients = null)
+    protected function directSend($to, &$failedRecipients = null, $message = null)
     {
-        $message = $this->setupSwiftMessage($to, $this->email->getFieldCc(), $this->email->getFieldBcc());
-
+        $message = $this->setupSwiftMessage($to, $message);
         $replacements = new Replacements($this->manager);
         $decorator = new \Swift_Plugins_DecoratorPlugin($replacements);
+        
         $this->directMailer->registerPlugin($decorator);
-
         $sent = $this->directMailer->send($message, $failedRecipients);
         $this->updateEmailEntity($message);
 
