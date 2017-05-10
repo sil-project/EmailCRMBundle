b5d63df698c23573808d4652e28477bab68b8f39
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2017-05-10 15:42:42 +0200

    minor

--- src/Admin/OrganismAdmin.php
+++ src/Admin/OrganismAdmin.php
@@ -11,19 +11,9 @@
 namespace Librinfo\EmailCRMBundle\Admin;
 
 use Librinfo\CRMBundle\Admin\OrganismAdmin as BaseOrganismAdmin;
-use Sonata\AdminBundle\Show\ShowMapper;
 
 class OrganismAdmin extends BaseOrganismAdmin
 {
     protected $baseRouteName = 'admin_librinfo_emailcrm_organism';
     protected $baseRoutePattern = 'librinfo/emailcrm/organism';
-
-    /**
-     * @param ShowMapper $mapper
-     */
-    protected function configureShowFields(ShowMapper $mapper)
-    {
-        parent::configureShowFields($mapper);
-        $mapper->get('positions')->setTemplate('LibrinfoEmailCRMBundle:CRUD:show_field_organism_positions.html.twig');
-    }
 }
